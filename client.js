eb6b9d61171042d4586fb4dedd405c4bff52c798
Nathan M <nathanmasol@gmail.com>
2019-10-22 11:11:13 -0500

    Ready pa entrega 1?

--- client.js
+++ client.js
@@ -1,82 +1,103 @@
-const socket = io('http://192.168.0.15:3000');
-//Chicos la ip aqui tambien cambia, son la misma ip en ambos archivos
-const noSeUser = document.querySelector('.deficionUsuario');
-const clienteUser = document.querySelector('.usuarioCliente');
-const asesorUser = document.querySelector('.usuarioAsesor');
-const enviarUsuario = document.querySelector('.enviarUsuario');
-const selectUser = document.querySelector('.selectorUsuario');
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;//compatibility for Firefox and chrome
+var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};      
+pc.createDataChannel('');//create a bogus data channel
+pc.createOffer(pc.setLocalDescription.bind(pc), noop);// create offer and set local description
+pc.onicecandidate = function(ice)
+{
+ if (ice && ice.candidate && ice.candidate.candidate)
+ {
+  var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+  console.log('my IP: ', myIP); 
+  
+    iniciarEnlace(myIP);
 
-const infoForm = document.querySelector('.formularioCliente');
-const enviar = document.querySelector('.enviar');
+  pc.onicecandidate = noop;
+ }
+};
 
-const infoShow = document.querySelector('.informacionCliente');
+function iniciarEnlace(myIP){
 
+    const socket = io('http://'+myIP+':3000');
+    //Chicos la ip aqui tambien cambia, son la misma ip en ambos archivos
+    const noSeUser = document.querySelector('.deficionUsuario');
+    const clienteUser = document.querySelector('.usuarioCliente');
+    const asesorUser = document.querySelector('.usuarioAsesor');
+    const enviarUsuario = document.querySelector('.enviarUsuario');
+    const selectUser = document.querySelector('.selectorUsuario');
+    
+    const infoForm = document.querySelector('.formularioCliente');
+    const enviar = document.querySelector('.enviar');
+    
+    const infoShow = document.querySelector('.informacionCliente');
+    
 
-function definicionUsuario(){
-    let usuario = selectUser.value;
-
-    if(usuario === 'Cliente'){
-        noSeUser.style.display= 'none';
-        clienteUser.style.display= 'block';
-    }else{
-        noSeUser.style.display= 'none';
-        asesorUser.style.display= 'block';
+    function definicionUsuario(){
+        let usuario = selectUser.value;
+    
+        if(usuario === 'Cliente'){
+            noSeUser.style.display= 'none';
+            clienteUser.style.display= 'block';
+        }else{
+            noSeUser.style.display= 'none';
+            asesorUser.style.display= 'block';
+        }
     }
-}
-enviarUsuario.addEventListener('click', definicionUsuario);
-
-socket.on('envioAlCliente', data => {
-    mostrarDatos(data);
-});
-
-enviar.addEventListener('click', e => {
-    console.log('enviando');
-    var data = {
-        nombre: infoForm.nombre.value,
-        cedula: infoForm.cedula.value,
-        celular: infoForm.celular.value,
-        motivo: infoForm.motivo.value,
-    }
-
-    socket.emit('enviarALServer', data);
-    data = ''; 
-
-})
-
-function mostrarDatos(data) {
-    console.log(data);
-    if(data.nombre !== undefined){
-        const tabla = document.createElement('table');
-        const titular1 = document.createElement('th');
-        titular1.innerText = 'Nombre';
-        const titular2 = document.createElement('th');
-        titular2.innerText = 'Cedula';
-        const titular3 = document.createElement('th');
-        titular3.innerText = 'Celular';
-        const titular4 = document.createElement('th');
-        titular4.innerText = 'Motivo de visita';
-        const fila = document.createElement('tr');
-        const casillaNombre = document.createElement('td');
-        casillaNombre.innerText = data.nombre;
-        const casillaCedula = document.createElement('td');
-        casillaCedula.innerText = data.cedula;
-        const casillaCelular = document.createElement('td');
-        casillaCelular.innerText = data.celular;
-        const casillaMotivo = document.createElement('td');
-        casillaMotivo.innerText = data.motivo;
+    enviarUsuario.addEventListener('click', definicionUsuario);
     
-        tabla.appendChild(titular1);
-        tabla.appendChild(titular2);
-        tabla.appendChild(titular3);
-        tabla.appendChild(titular4);
+    socket.on('envioAlCliente', data => {
+        mostrarDatos(data);
+    });
     
-        fila.appendChild(casillaNombre);
-        fila.appendChild(casillaCedula);
-        fila.appendChild(casillaCelular);
-        fila.appendChild(casillaMotivo);
+    enviar.addEventListener('click', e => {
+        console.log('enviando');
+        var data = {
+            nombre: infoForm.nombre.value,
+            cedula: infoForm.cedula.value,
+            celular: infoForm.celular.value,
+            motivo: infoForm.motivo.value,
+        }
     
-        infoShow.appendChild(tabla);
-        tabla.appendChild(fila);
+        socket.emit('enviarALServer', data);
+        data = ''; 
+    
+    })
+    
+    function mostrarDatos(data) {
+        console.log(data);
+        if(data.nombre !== undefined){
+            const tabla = document.createElement('table');
+            const titular1 = document.createElement('th');
+            titular1.innerText = 'Nombre';
+            const titular2 = document.createElement('th');
+            titular2.innerText = 'Cedula';
+            const titular3 = document.createElement('th');
+            titular3.innerText = 'Celular';
+            const titular4 = document.createElement('th');
+            titular4.innerText = 'Motivo de visita';
+            const fila = document.createElement('tr');
+            const casillaNombre = document.createElement('td');
+            casillaNombre.innerText = data.nombre;
+            const casillaCedula = document.createElement('td');
+            casillaCedula.innerText = data.cedula;
+            const casillaCelular = document.createElement('td');
+            casillaCelular.innerText = data.celular;
+            const casillaMotivo = document.createElement('td');
+            casillaMotivo.innerText = data.motivo;
+        
+            tabla.appendChild(titular1);
+            tabla.appendChild(titular2);
+            tabla.appendChild(titular3);
+            tabla.appendChild(titular4);
+        
+            fila.appendChild(casillaNombre);
+            fila.appendChild(casillaCedula);
+            fila.appendChild(casillaCelular);
+            fila.appendChild(casillaMotivo);
+        
+            infoShow.appendChild(tabla);
+            tabla.appendChild(fila);
+        }
+    
     }
 
 }
