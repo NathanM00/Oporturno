bee3857346232dbb1c68d06e22dac5829d616b6e
MinottaDeveloper <42478783+MinottaDeveloper@users.noreply.github.com>
2019-11-05 12:04:10 -0500

    Styles

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://192.168.0.14:3000');
+const socket = io('http://172.30.170.77:3000');
 //Chicos la ip aqui tambien cambia, son la misma ip en ambos archivos
 
 const enviarUsuario = document.querySelector('.enviarUsuario');
